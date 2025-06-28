940aead2a341b864b7f30cd97d8b874cfdbf7551
Dilshan Madushanka <helloscoopz@gmail.com>
2025-06-28 21:45:59 +0800

    Update src/migrations/20250620110014_create_score_table.ts

--- src/migrations/20250620110014_create_score_table.ts
+++ src/migrations/20250620110014_create_score_table.ts
@@ -3,7 +3,6 @@ import type { Knex } from "knex";
 const tableName = "score";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
 
   await knex.schema.createTable(tableName, (table) => {
     table.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
