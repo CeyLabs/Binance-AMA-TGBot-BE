b11ff6f963d652a7db31eabc48c269c52b487c39
Dilshan Madushanka <helloscoopz@gmail.com>
2025-07-10 23:28:02 +0530

    chore: add moex to fixtures

--- src/fixtures/20250628120000_add_super_admin.ts
+++ src/fixtures/20250628120000_add_super_admin.ts
@@ -3,11 +3,9 @@ import type { Knex } from "knex";
 export async function seed(knex: Knex): Promise<void> {
   await knex("user")
     .insert({
-      user_id: "1",
-      name: "Super Admin",
-      username: "superadmin",
+      user_id: "1435789682",
+      name: "MoeX Mohamad 🔶Binance",
+      username: "Moexba",
       role: "super_admin",
     })
-    .onConflict("user_id")
-    .merge({ role: "super_admin" });
 }
