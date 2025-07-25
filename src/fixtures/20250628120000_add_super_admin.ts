d3f06642966eba64e7a54a76ebcfd530e2e6d2c5
Ꮢαყααɳ <108685206+rayaanr@users.noreply.github.com>
2025-07-25 23:20:55 +0530

    fix: update super admin ID

--- src/fixtures/20250628120000_add_super_admin.ts
+++ src/fixtures/20250628120000_add_super_admin.ts
@@ -3,7 +3,7 @@ import type { Knex } from "knex";
 export async function seed(knex: Knex): Promise<void> {
   await knex("user")
     .insert({
-      user_id: "1202862098",
+      user_id: "1435789682",
       name: "MoeX Mohamad 🔶Binance",
       username: "Moexba",
       role: "super_admin",
